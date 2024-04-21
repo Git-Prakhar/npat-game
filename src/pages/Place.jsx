bdb508add991b76a54cfae78dc8f5a5fba6e2223
Rutetid <50543186+Rutetid@users.noreply.github.com>
2024-04-22 01:14:27 +0530

    Update Place.jsx

--- src/pages/Place.jsx
+++ src/pages/Place.jsx
@@ -1,88 +1,88 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+    import React, { useEffect, useRef, useState } from 'react'
+    import { useNavigate } from 'react-router-dom';
 
-export default function Place({ round, score, setScore }) {
+    export default function Place({ round, score, setScore }) {
 
-    const [options, setOptions] = useState([]);
-    const navigate = useNavigate();
-    const [tempScore, setTempScore] = useState(1000);
-    const timerRef = useRef(null);
+        const [options, setOptions] = useState([]);
+        const navigate = useNavigate();
+        const [tempScore, setTempScore] = useState(1500);
+        const timerRef = useRef(null);
 
-    // Functions
-    function generateOptions() {
-        let placeOptions = [];
-        placeOptions[round[0].correctOption] = round[0].correctURL.place;
-        let count = 0;
-        if (round[0].correctOption === 0) {
-            count = 1;
+        // Functions
+        function generateOptions() {
+            let placeOptions = [];
+            placeOptions[round[0].correctOption] = round[0].correctURL.place;
+            let count = 0;
+            if (round[0].correctOption === 0) {
+                count = 1;
+            }
+            round[0].options.place.forEach((option) => {
+                if (count !== round[0].correctOption) {
+                    placeOptions[count] = option;
+                }
+                if ((count + 1) === round[0].correctOption) {
+                    count += 2;
+                } else {
+                    count++;
+                }
+            });
+            setOptions(placeOptions);
         }
-        round[0].options.place.forEach((option) => {
-            if (count !== round[0].correctOption) {
-                placeOptions[count] = option;
-            }
-            if ((count + 1) === round[0].correctOption) {
-                count += 2;
-            } else {
-                count++;
-            }
-        });
-        setOptions(placeOptions);
-    }
 
-    const handleOptionSelected = (index) => {
-        clearInterval(timerRef.current);
-        let temp = score;
-        if(index === round[0].correctOption){
-            temp += tempScore;
-            setScore(temp);
+        const handleOptionSelected = (index) => {
+            clearInterval(timerRef.current);
+            let temp = score;
+            if(index === round[0].correctOption){
+                temp += tempScore;
+                setScore(temp);
+            }
+            navigate('/game/animal');
         }
-        navigate('/game/animal');
-    }
 
-    const startTimer = () => {
-        let temp = 1000;
-        timerRef.current = setInterval(() => {
-            console.log('Passing');
-            temp -= 35;
-            setTempScore(temp);
-            if (temp <= 0) {
-                clearInterval(timerRef.current);
-                navigate('/game/animal');
-            }
-        }, 100);
-    }
+        const startTimer = () => {
+            let temp = 1500;
+            timerRef.current = setInterval(() => {
+                console.log('Passing');
+                temp -= 35;
+                setTempScore(temp);
+                if (temp <= 0) {
+                    clearInterval(timerRef.current);
+                    navigate('/game/animal');
+                }
+            }, 100);
+        }
 
-    // UseEffect
+        // UseEffect
 
-    useEffect(() => {
-        if (!round) return navigate('/');
-        generateOptions();
-        startTimer();
-    }, [])
+        useEffect(() => {
+            if (!round) return navigate('/');
+            generateOptions();
+            startTimer();
+        }, [])
 
-    return (
-        <>
-            {
-                round &&
-                <div id='game-page' className='df-col aic'>
-                    <div className='df jcc aic' id='nav'>
-                        <div id='game-name'>Place</div>
-                        <div id='timing'>{tempScore}</div>
-                        <div id='letter'>{round[0].roundLetter}</div>
+        return (
+            <>
+                {
+                    round &&
+                    <div id='game-page' className='df-col aic'>
+                        <div className='df jcc aic' id='nav'>
+                            <div id='game-name'>Place</div>
+                            <div id='timing'>{tempScore}</div>
+                            <div id='letter'>{round[0].roundLetter}</div>
+                        </div>
+                        <div className="choiceHolder df jcc aic" >
+                            {
+                                options && options.map((option, index) => {
+                                    return (
+                                        <button onClick={() => { handleOptionSelected(index) }} key={index} className='choice df jcc aic'>
+                                            <img src={option} alt={option} />
+                                        </button>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
-                    <div className="choiceHolder df jcc aic" >
-                        {
-                            options && options.map((option, index) => {
-                                return (
-                                    <button onClick={() => { handleOptionSelected(index) }} key={index} className='choice df jcc aic'>
-                                        <img src={option} alt={option} />
-                                    </button>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            }
-        </>
-    )
-}
+                }
+            </>
+        )
+    }
